a42bf58bc5ccbd7e21f450b7673ae7686e6687cd
adnan <alhossain.a92@gmail.com>
2021-12-15 06:33:08 -0800

    form added

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+ 
+
+<div class="row">
+  <div class="col-sm-8 m-auto">
+    <div class="card">
+    <div class="card-body">
+      	<div class="card  mb-3">
+  <div class="card-header text-white bg-primary">Student</div>
+
+       <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Name</label>
+    <input type="text" class="form-control" id="" name="name" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Roll</label>
+    <input type="text" class="form-control" name="roll" id="">
+  </div>
+    <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Age</label>
+    <input type="text" class="form-control" name="age" id="exampleInputPassword1">
+  </div>  
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</div>
+
+
+
+    
+    </div>
+    </div>
+
+  </div>
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
